f7cc9a8dafd45480273ddc5fb11ac8c3578ab53c
Mridul <mridulpal19@gmail.com>
2022-08-25 00:55:07 +0530

    add UI

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+import {data} from "../data"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">
+            Movies
+          </div>
+          <div className="tab">
+              Favourites
+          </div>
+        </div>
+        <div className="list">
+            {data.map(movie=>
+              <MovieCard  movie={movie}/>
+              )}
+
+        </div>
+      </div>
+       Project Setup
+    </div>
+  );
+}
+
+export default App;
